da6da3e69637ef1fc03b0b448258e81e697ada1b
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2024-09-05 14:48:05 +0700

    Done

---
+++ src/components/YourRating.tsx
@@ -0,0 +1,13 @@
+import { Text, Rating } from "@mantine/core";
+
+export default function YourRating() {
+  return (
+    <div>
+      <Text size="lg" fw="bold">
+        Your rating
+      </Text>
+      <Rating defaultValue={0} size="lg" mb="sm" />
+    </div>
+  )
+}
+
